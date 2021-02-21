2021d9f939f4f06145b6c0c8ae4b64e5973f9164
Zeina Thabet <zeina.thabet@outlook.com>
2021-02-21 20:15:12 +0400

    added partial initialization in main

--- src/delivery/UI/swing/Main.java
+++ src/delivery/UI/swing/Main.java
@@ -1,6 +1,11 @@
 package delivery.UI.swing;
 
+import delivery.domain.SystemController;
+
 public class Main {
-    //
+    // inittialze the controller
+    SystemController controller = new SystemController();
+
+    // then in the main Jframe (start page) pass in the controller object
 
 }
