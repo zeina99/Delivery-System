0e4c258901854971beb46ddabd6236b2fb250643
Zeina Thabet <zeina.thabet@outlook.com>
2021-02-21 20:45:44 +0400

    typo

--- src/delivery/UI/swing/Main.java
+++ src/delivery/UI/swing/Main.java
@@ -3,7 +3,7 @@ package delivery.UI.swing;
 import delivery.domain.SystemController;
 
 public class Main {
-    // inittialze the controller
+    // initialize the controller
     SystemController controller = new SystemController();
 
     // then in the main Jframe (start page) pass in the controller object
