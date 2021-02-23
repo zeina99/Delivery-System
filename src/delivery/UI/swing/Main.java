07fbb180abcfdae27ca282277cc4ea543216e3af
Zeina Thabet <zeina.thabet@outlook.com>
2021-02-23 15:47:56 +0400

    added main method

--- src/delivery/UI/swing/Main.java
+++ src/delivery/UI/swing/Main.java
@@ -3,9 +3,12 @@ package delivery.UI.swing;
 import delivery.domain.SystemController;
 
 public class Main {
-    // initialize the controller
+    // initialize the controller from domain layer
     SystemController controller = new SystemController();
 
-    // then in the main Jframe (start page) pass in the controller object
+    public static void main(String[] args) {
+
+        // then in the main Jframe (start page) initialization pass in the controller object
+    }
 
 }
