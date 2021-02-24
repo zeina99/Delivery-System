b3cceae38b8ef43b7ab61ae25cadd83886af33ae
Zeina Thabet <zeina.thabet@outlook.com>
2021-02-24 23:36:46 +0400

    commit all

--- src/delivery/technicalServices/persistence/DriverDAO.java
+++ src/delivery/technicalServices/persistence/DriverDAO.java
@@ -1,33 +1,4 @@
 package delivery.technicalServices.persistence;
 
-import delivery.domain.Driver;
-
-import java.util.List;
-
-public class DriverDAO implements GenericDAO<Driver>{
-
-    @Override
-    public void insert(Driver object) {
-
-    }
-
-    @Override
-    public void update(Driver object) {
-
-    }
-
-    @Override
-    public void delete(Driver object) {
-
-    }
-
-    @Override
-    public Driver getById(int pk) {
-        return null;
-    }
-
-    @Override
-    public List<Driver> getAll() {
-        return null;
-    }
+public class DriverDAO {
 }
