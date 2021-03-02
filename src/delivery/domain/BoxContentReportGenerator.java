3c02670a274ba4aa0fa2b45f08643e0d2594aa3d
Zeina Thabet <zeina.thabet@outlook.com>
2021-03-02 12:54:32 +0400

    refactored BoxContentReportGenerator into more methods and extracted variables into class fields

--- src/delivery/domain/BoxContentReportGenerator.java
+++ src/delivery/domain/BoxContentReportGenerator.java
@@ -4,14 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoxContentReportGenerator {
+
     private final ReportGenerator reportGenerator;
+    private Inventory inventory = new Inventory();
+    // box id
+    private int boxID;
+    private List<Box> boxList;
+    private final List<Double> boxVolumes;
+    private int boxVolumesIndex;
+    private int orderItemsIndex;
+    private double totalOrderVolume;
+
 
     public BoxContentReportGenerator(ReportGenerator reportGenerator) {
+
         this.reportGenerator = reportGenerator;
+        this.boxID = 1;
+        this.boxList = new ArrayList<>();
+
+        // allBoxSizes Sorted descending , starting with largest box size
+        this.boxVolumes = inventory.getAllBoxVolumesSorted();
+        this.boxVolumesIndex = 0;
+        this.orderItemsIndex = 0;
+
     }
 
     public List<Box> generateBoxContentList() {
-        List<Box> boxList = new ArrayList<>();
+
         //TODO: refactor this into a class and methods
         //Map {
         // TimeSlot: [order, order, order, order]
@@ -23,117 +42,139 @@ public class BoxContentReportGenerator {
         // max orders per van is 9
         int timeslotMaxOrders = 9 * numOfVans;
 
-        // box id
-        int boxID = 1;
 
         // loop over all time slots
         for (TimeSlots timeslot :
                 reportGenerator.getOrderMap().keySet()) {
 
-            List<Order> orders = reportGenerator.getOrderMap().get(timeslot);
+            // get orders for current timeslot
+            List<Order> timeslotOrders = inventory.getAllOrdersMap().get(timeslot);
 
             // defer extra orders to unaccomodated list
-            deferExtraOrders(timeslotMaxOrders, orders);
+            deferExtraOrders(timeslotMaxOrders, timeslotOrders);
 
             // loop over array of orders for curr timeslot
             // here we try to fill boxes with order items for each order
-            boxID = fillOrderItemsInBox(boxID, orders, boxList);
+            fillOrderItemsInBox(timeslotOrders);
 
 
         }
         return boxList;
     }
 
-    private int fillOrderItemsInBox(int boxID, List<Order> orders, List<Box> boxList) {
+    private void fillOrderItemsInBox(List<Order> timeslotOrders) {
+
+        // looping over each order
         for (Order order :
-                orders) {
+                timeslotOrders) {
 
-            // getting total volume of order
-            double totalOrderVolume = getTotalOrderVolume(order);
-
-            // picking boxes for order
-            // allBoxSizes Sorted descending , starting with largest box size
-            List<Double> allBoxSizes = reportGenerator.getInventory().getAllBoxVolumesSorted();
-            int allBoxSizesIndex = 0;
 
             // order items descending by item volume
             List<OrderItem> orderItemsSortedByVolume = order.getItemsSortedDesc();
-            int orderItemsIndex = 0;
 
-            // while loop to fill boxes with current order items
-            boxID = fillBoxes(boxID, boxList, totalOrderVolume, allBoxSizes, allBoxSizesIndex, orderItemsSortedByVolume, orderItemsIndex);
+            totalOrderVolume = getTotalOrderVolume(order);
+            // fills order items in boxes
+            fillAllOrderItems(orderItemsSortedByVolume);
 
 
         }
-        return boxID;
     }
 
-    private int fillBoxes(int boxID, List<Box> boxList, double totalOrderVolume, List<Double> allBoxSizes, int allBoxSizesIndex, List<OrderItem> orderItemsSortedByVolume, int orderItemsIndex) {
 
-        double boxVolRemaining;
+    private void fillAllOrderItems(List<OrderItem> orderItemsSortedByVolume) {
+
+        // refactor this method further,
+        // method to return the suitable box volume
+        // method to fill the items in the box
+        // getting total volume of order
+
+
+        orderItemsIndex = 0;
+
 
         while (totalOrderVolume > 0) {
+            // -- picking a box
+
+
             //--- get a box just greater than total order volume in if statements ---
 
-            // if total order volume is less than current box size and greater than next box size, get curr box
-            if (totalOrderVolume < allBoxSizes.get(allBoxSizesIndex) && (totalOrderVolume > allBoxSizes.get(allBoxSizesIndex + 1)))
-            {
-                // start of box filling, starting box volume
-                boxVolRemaining = allBoxSizes.get(allBoxSizesIndex);
-                // remaining order volume reduced according to box volume selected
-                totalOrderVolume =- allBoxSizes.get(allBoxSizesIndex);
+            // if the next smaller box is smaller than current total order volume, continue
+            // else, pick the current one
+            // if we are at the smallest box, use it
 
-                // start filling order items to box until box is full
-                while (boxVolRemaining > 0) {
 
-                    // creating and adding box to list
-                    Box box = new Box(boxID, reportGenerator.getInventory().getBoxDescriptionFromVolume(boxVolRemaining));
-                    boxList.add(box);
-                    boxID++;
-                    // adding items to box
-                    box.addItem(orderItemsSortedByVolume.get(orderItemsIndex));
-                    //orderItemsSortedByVolume.remove(orderItemsIndex);
-                    boxVolRemaining =- orderItemsSortedByVolume.get(orderItemsIndex).getVolume();
-                    orderItemsIndex++;
-                }
+            // pick a suitable size box
+            Double boxVolRemaining = getBoxVol(totalOrderVolume);
 
-                allBoxSizesIndex = 0;
-            }
 
-            // we're at the largest box size, use it
-            if (allBoxSizesIndex + 1 == allBoxSizes.size()) {
-                boxVolRemaining = allBoxSizes.get(allBoxSizesIndex);
-                totalOrderVolume = -allBoxSizes.get(allBoxSizesIndex);
+            // fills a box with items
+            fillBoxWithItems(boxVolRemaining, orderItemsSortedByVolume);
 
-                // TODO: duplicate code
-                while (boxVolRemaining > 0) {
 
-                    // creating and adding box to list
-                    Box box = new Box(boxID, reportGenerator.getInventory().getBoxDescriptionFromVolume(boxVolRemaining));
-                    boxList.add(box);
-                    boxID++;
-                    // adding items to box
-                    box.addItem(orderItemsSortedByVolume.get(orderItemsIndex));
-                    //orderItemsSortedByVolume.remove(orderItemsIndex);
-                    boxVolRemaining =- orderItemsSortedByVolume.get(orderItemsIndex).getVolume();
-                    orderItemsIndex++;
-                }
-                allBoxSizesIndex = 0;
-            }
-
-            allBoxSizesIndex++;
+            this.boxVolumesIndex = 0;
         }
-        return boxID;
+
+    }
+
+    private Double getBoxVol(double totalOrderVolume) {
+
+        if (this.boxVolumesIndex + 1 == this.boxVolumes.size()) {
+            return this.boxVolumes.get(boxVolumesIndex);
+        } else if (totalOrderVolume < boxVolumes.get(boxVolumesIndex + 1)) {
+            // avoid getting out of index
+            while (boxVolumesIndex + 1 != boxVolumes.size() && totalOrderVolume < boxVolumes.get(boxVolumesIndex + 1)) {
+                this.boxVolumesIndex++;
+            }
+            return this.boxVolumes.get(boxVolumesIndex); // - 1
+        } else {
+            return this.boxVolumes.get(this.boxVolumesIndex);
+        }
+
+    }
+
+    private void fillBoxWithItems(double boxVolRemaining, List<OrderItem> orderItemsSortedByVolume) {
+
+
+        Box box = new Box(boxID, inventory.getBoxDescriptionFromVolume(boxVolRemaining));
+        // creating and adding box to list
+        this.boxList.add(box);
+        this.boxID++;
+
+        // start filling order items to box until box is full
+        // while box can still fit the next item in line
+
+
+        // while box can still fit next item in line AND we are not at the last item in order items
+        while (this.orderItemsIndex != orderItemsSortedByVolume.size() && boxVolRemaining >= orderItemsSortedByVolume.get(this.orderItemsIndex).getVolume()) {
+
+
+            OrderItem itemToAddToBox = orderItemsSortedByVolume.get(this.orderItemsIndex);
+            // reduce remaining order volume with volume of item added to box RECENTLY ADDED -------------------
+            // adding items to box
+            box.addItem(itemToAddToBox);
+            totalOrderVolume -= itemToAddToBox.getVolume(); //-------------------------
+            boxVolRemaining -= itemToAddToBox.getVolume();
+
+            this.orderItemsIndex++;
+
+//            if (this.orderItemsIndex+1 == orderItemsSortedByVolume.size()) {
+//                break;
+//            }
+//                    nextItemVolume = orderItemsSortedByVolume.get(orderItemsIndex + 1).getVolume();
+        }
+
+
+        this.boxVolumesIndex++;
     }
 
     private double getTotalOrderVolume(Order order) {
         // get total volume of order
-        double totalOrderVolume = 0;
+        double orderVolume = 0;
         for (OrderItem item :
                 order.getItems()) {
-            totalOrderVolume += item.getCategory().getVolume();
+            orderVolume += item.getCategory().getVolume();
         }
-        return totalOrderVolume;
+        return orderVolume;
     }
 
     private void deferExtraOrders(int timeslotMaxOrders, List<Order> orders) {
@@ -149,3 +190,118 @@ public class BoxContentReportGenerator {
         }
     }
 }
+
+
+//            if (this.allBoxSizesIndex + 1 == this.allBoxSizes.size()) {
+//                // start of box filling, starting box volume
+//                boxVolRemaining = this.allBoxSizes.get(allBoxSizesIndex);
+//                // remaining order volume reduced according to box volume selected
+//                totalOrderVolume -= boxVolRemaining;
+//
+//                // creating and adding box to list
+//                Box box = new Box(boxID, inventory.getBoxDescriptionFromVolume(boxVolRemaining));
+//                this.boxList.add(box);
+//                this.boxID++;
+//
+//
+//                // start filling order items to box until box is full
+//                // while box can still fit next item in line AND we are not at the last item in order items
+////                if ()
+//                while (orderItemsIndex + 1 != orderItemsSortedByVolume.size() && boxVolRemaining >= orderItemsSortedByVolume.get(orderItemsIndex + 1).getVolume()) {
+//
+//
+//                    OrderItem itemToAddToBox = orderItemsSortedByVolume.get(orderItemsIndex);
+//                    // adding items to box
+//                    box.addItem(itemToAddToBox);
+//                    //orderItemsSortedByVolume.remove(orderItemsIndex);
+//                    boxVolRemaining -= itemToAddToBox.getVolume();
+//
+//                    orderItemsIndex++;
+//
+////                    nextItemVolume = orderItemsSortedByVolume.get(orderItemsIndex + 1).getVolume();
+//                }
+//
+//                allBoxSizesIndex++;
+//
+//            } else if (totalOrderVolume < allBoxSizes.get(allBoxSizesIndex + 1)) {
+//                this.allBoxSizesIndex++;
+//                continue;
+//            } else {
+//                // start of box filling, starting box volume
+//                boxVolRemaining = allBoxSizes.get(allBoxSizesIndex);
+//                // remaining order volume reduced according to box volume selected
+//                totalOrderVolume -= boxVolRemaining;
+//
+//                Box box = new Box(boxID, inventory.getBoxDescriptionFromVolume(boxVolRemaining));
+//                // creating and adding box to list
+//                this.boxList.add(box);
+//                this.boxID++;
+//
+//                // start filling order items to box until box is full
+//                // while box can still fit the next item in line
+//
+//
+//
+//                // while box can still fit next item in line AND we are not at the last item in order items
+//                while (orderItemsIndex + 1 != orderItemsSortedByVolume.size() && boxVolRemaining >= orderItemsSortedByVolume.get(orderItemsIndex + 1).getVolume()) {
+//
+//
+//                    OrderItem itemToAddToBox = orderItemsSortedByVolume.get(orderItemsIndex);
+//                    // adding items to box
+//                    box.addItem(itemToAddToBox);
+//                    //orderItemsSortedByVolume.remove(orderItemsIndex);
+//                    boxVolRemaining -= itemToAddToBox.getVolume();
+//
+//                    orderItemsIndex++;
+//
+////                    nextItemVolume = orderItemsSortedByVolume.get(orderItemsIndex + 1).getVolume();
+//                }
+//
+//
+//                allBoxSizesIndex++;
+//            }
+//            // if total order volume is less than current box size and greater than next box size, get curr box
+//            if (totalOrderVolume < allBoxSizes.get(allBoxSizesIndex) && (totalOrderVolume > allBoxSizes.get(allBoxSizesIndex + 1)))
+//            {
+//                // start of box filling, starting box volume
+//                boxVolRemaining = allBoxSizes.get(allBoxSizesIndex);
+//                // remaining order volume reduced according to box volume selected
+//                totalOrderVolume =- allBoxSizes.get(allBoxSizesIndex);
+//
+//                // start filling order items to box until box is full
+//                while (boxVolRemaining > 0) {
+//
+//                    // creating and adding box to list
+//                    Box box = new Box(boxID, inventory.getBoxDescriptionFromVolume(boxVolRemaining));
+//                    boxList.add(box);
+//                    boxID++;
+//                    // adding items to box
+//                    box.addItem(orderItemsSortedByVolume.get(orderItemsIndex));
+//                    //orderItemsSortedByVolume.remove(orderItemsIndex);
+//                    boxVolRemaining =- orderItemsSortedByVolume.get(orderItemsIndex).getVolume();
+//                    orderItemsIndex++;
+//                }
+//
+//                allBoxSizesIndex = 0;
+//            }
+//
+//            // we're at the largest box size, use it
+//            if (allBoxSizesIndex + 1 == allBoxSizes.size()) {
+//                boxVolRemaining = allBoxSizes.get(allBoxSizesIndex);
+//                totalOrderVolume = -allBoxSizes.get(allBoxSizesIndex);
+//
+//
+//                while (boxVolRemaining > 0) {
+//
+//                    // creating and adding box to list
+//                    Box box = new Box(boxID, inventory.getBoxDescriptionFromVolume(boxVolRemaining));
+//                    boxList.add(box);
+//                    boxID++;
+//                    // adding items to box
+//                    box.addItem(orderItemsSortedByVolume.get(orderItemsIndex));
+//                    //orderItemsSortedByVolume.remove(orderItemsIndex);
+//                    boxVolRemaining =- orderItemsSortedByVolume.get(orderItemsIndex).getVolume();
+//                    orderItemsIndex++;
+//                }
+//                allBoxSizesIndex = 0;
+//            }
